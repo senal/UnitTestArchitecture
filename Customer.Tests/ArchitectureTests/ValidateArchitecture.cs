5d48e0438aaef15bcb8f2666b164f310cefd5a2c
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-03-14 15:55:26 +1100

    Add central package management capability

--- Customer.Tests/ArchitectureTests/ValidateArchitecture.cs
+++ Customer.Tests/ArchitectureTests/ValidateArchitecture.cs
@@ -1,9 +1,8 @@
-using ArchUnitNET.Domain;
-using ArchUnitNET.Loader;
-
 namespace Customer.Tests.ArchitectureTests;
 
 public class ValidateArchitecture
 {
-    private static readonly Architecture architecture = new ArchLoader().LoadAssemblies(typeof(Program)).Build();
+    // private static readonly Architecture architecture = new ArchLoader()
+    //     .LoadAssemblies(typeof(WeatherForecastController).Assembly, typeof())
+    //     .Build();
 }
